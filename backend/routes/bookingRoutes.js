bf0d74ffdf51f96cd41b0ff9e7b7144f190296d4
Agaba Valentine <146995622+Abobbynwa@users.noreply.github.com>
2026-05-20 13:27:32 +0000

    Add bank transfer proof route

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -3,14 +3,16 @@ import {
   createBooking,
   getBookings,
   getBooking,
-  updateBooking
+  updateBooking,
+  submitTransferProof
 } from '../controllers/bookingController.js';
 import { protect, admin } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.route('/').post(createBooking).get(protect, admin, getBookings);
-router.route('/:reference').get(getBooking);
+router.post('/transfer-proof', submitTransferProof);
+router.route('/reference/:reference').get(getBooking);
 router.route('/:id').patch(protect, admin, updateBooking);
 
 export default router;
